b297fa84508bb44d8db2d60732ff1f0823826a1c
KaranTikellu7 <tikellukaran@gmail.com>
2018-10-25 16:38:26 +0530

    New File of TESTRUN

---
+++ AceInvoice_Selenium/src/TestCases/TestRUN1.java
@@ -0,0 +1,22 @@
+package TestCases;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class TestRUN1 {
+	
+	static WebDriver driver;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Desktop\\selenium 64 bit\\chromedriver.exe");
+	driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+	driver.get("https://www.facebook.com/");
+	
+//	driver.close();
+	}
+
+}
